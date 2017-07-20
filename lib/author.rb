90c45ff79d6d836167c7f1e9aebb1f5f626ceec4
linkcoding <kenny.lin.91@gmail.com>
2017-07-20 19:46:32 -0400

    add classes artist, song, author, and post

---
+++ lib/author.rb
@@ -0,0 +1,25 @@
+class Author
+  attr_accessor :posts, :name
+
+  @@post_count = 0
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    post.author = self
+    posts << post
+    @@post_count += 1
+  end
+
+  def add_post_by_title(title)
+    new_title = Post.new(title)
+    self.add_post(new_title)
+  end
+
+  def self.post_count
+    @@post_count
+  end
+end
